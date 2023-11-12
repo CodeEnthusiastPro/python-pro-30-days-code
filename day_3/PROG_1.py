eac235c0f52af1955d25952d8c71d130e293d341
CodeEnthusiastPro <150283721+CodeEnthusiastPro@users.noreply.github.com>
2023-11-11 19:34:29 -0800

    Add files via upload

---
+++ day_3/PROG_1.py
@@ -0,0 +1,10 @@
+my_favorite_fruits = {"watermelon", "apple", "mango"}
+
+print("The Original set of my favorite fruits:", my_favorite_fruits)
+
+# Add two more fruits to the set
+my_favorite_fruits.add("banana")
+my_favorite_fruits.add("strawberry")
+
+# Print the updated set
+print("Updated set of favorite fruits:", my_favorite_fruits)
