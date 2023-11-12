385c0997a86a274b352a40057d01dda0c539ae70
CodeEnthusiastPro <150283721+CodeEnthusiastPro@users.noreply.github.com>
2023-11-11 19:32:09 -0800

    Add files via upload

---
+++ day_1_2/PROG_1_EXPONENTIAL_COUNT.py
@@ -0,0 +1,15 @@
+x = int(input("Enter the value of x: "))
+y = int(input("Enter the value of y: "))
+
+
+result = x ** y
+
+num_digits = len(str(result))
+
+
+print(f"The result of x^y is: {result}")
+print(f"The number of digits in x^y is: {num_digits}")
+
+
+
+
